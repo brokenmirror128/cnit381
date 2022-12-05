ddb249fa37c0a3e3df3aafe1fd6cd2eabaaa8576
burntwelve <118204494+burntwelve@users.noreply.github.com>
2022-12-04 21:23:32 -0600

    Update and rename CPU_Line.py to Graphics.py
    updated to add Pie chart of memory usage

--- chatbot/Graphics.py
+++ chatbot/Graphics.py
@@ -36,3 +36,37 @@ class LiveLine:
       
 g = LiveLine()
 g.show()
+
+
+class Livepie:
+  def __init__(self):
+    self.data = [30,70]
+    self.figure, self.pie_chart = plt.subplots()
+    self.figure.suptitle('Memory Usage%', fontsize=18)
+    self.labels = 'Used', 'Free'
+    self.explod = (0, 0.1)
+    self.colors = ("Magenta", "Cyan")
+    self.pie_chart.axis('equal')
+    self.animation = FuncAnimation(self.figure, self.update, interval = 1000)
+    self.th = Thread(target=self.thread_f, daemon=True)
+    self.th.start()
+    
+  def update(self, frame):
+    self.pie_chart.pie(self.data, explode=self.explode,labels = self.labels, colors = self.colors,
+                       autopct='%1.1f%%', shadow=True, startangle=90)
+    self.figure.gca().relim()
+    self.figure.gca().autoscale_view()
+    return self.pie_chart,
+  
+  def show(self):
+    print('showing chart!')
+    
+  def thread_f(self):
+    while True:
+      self.data = get_mem()
+      plt.pause(0.001)
+      plt.draw()
+      time.sleep(1)
+      
+g = LivePie()
+g.show()
