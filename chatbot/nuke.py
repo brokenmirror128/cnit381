e317ba696304b1a259f6f720f13c6b760ed45fea
123rad567 <118935932+123rad567@users.noreply.github.com>
2022-12-04 23:16:40 -0600

    Update nuke.py

--- chatbot/nuke.py
+++ chatbot/nuke.py
@@ -1,6 +1,6 @@
 import paramiko 
-host1 = ''
-host2 = ''
+host1 = '172.16.0.1'
+host2 = '172.16.0.2'
 user = 'cisco'
 secret = 'Cisco123'
 port = 22
@@ -8,6 +8,12 @@ port = 22
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
 ssh.connect(hostname=host1, username=user, password=secret, port=port)
+stdin, stdout, stderr = ssh.exec_command('erase startup-config')
+stdin, stdout, stderr = ssh.exec_command('/n')
+stdin, stdout, stderr = ssh.exec_command('reload')
+stdin, stdout, stderr = ssh.exec_command('/n')
+list = stdout.readlines()
+ssh.connect(hostname=host2, username=user, password=secret, port=port)
 stdin, stdout, stderr = ssh.exec_command('erase startup-config')
 stdin, stdout, stderr = ssh.exec_command('/n')
 stdin, stdout, stderr = ssh.exec_command('reload')
