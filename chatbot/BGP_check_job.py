24486dab1cfce6a0aae21cd1fc779dd596edbcce
burntwelve <118204494+burntwelve@users.noreply.github.com>
2022-12-11 21:35:09 -0600

    Update BGP_check_job.py

--- chatbot/BGP_check_job.py
+++ chatbot/BGP_check_job.py
@@ -4,6 +4,6 @@ import os
 def main(runtime):
     # Find the location of the script in relation to the job file
     bgp_tests = os.path.join(os.path.dirname(__file__), 
-                             'BGP_Establish.py')
+                             'BGP_Neighbors_Established.py')
     # Execute the testscript
     runtime.tasks.run(testscript=testbed/routers)
