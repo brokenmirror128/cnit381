a406881149d940869fb41bcde10de87873553c98
burntwelve <118204494+burntwelve@users.noreply.github.com>
2022-12-11 23:13:07 -0600

    Create xrouters.py

---
+++ chatbot/xrouters.py
@@ -0,0 +1,13 @@
+router1 = {"host":"10.0.202.1",
+            "port":"830",
+            "username":"cisco",
+            "password":"cisco123!",
+            "hostkey_verify":"False"
+        }
+
+router2 = {"host":"10.0.202.2",
+            "port":"830",
+            "username":"cisco",
+            "password":"cisco123!",
+            "hostkey_verify":"False"
+        }
