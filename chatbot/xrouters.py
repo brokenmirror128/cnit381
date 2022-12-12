bf58feb728a5d1fa353610b02ac26afc0d0dbea5
burntwelve <118204494+burntwelve@users.noreply.github.com>
2022-12-11 23:19:38 -0600

    Update xrouters.py

--- chatbot/xrouters.py
+++ chatbot/xrouters.py
@@ -1,11 +1,11 @@
-router1 = {"host":"10.0.202.1",
+router1 = {"host":"1.1.1.1",
             "port":"830",
             "username":"cisco",
             "password":"cisco123!",
             "hostkey_verify":"False"
         }
 
-router2 = {"host":"10.0.202.2",
+router2 = {"host":"2.2.2.2",
             "port":"830",
             "username":"cisco",
             "password":"cisco123!",
